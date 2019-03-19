d7fc3fddf99fe5b1abd8ed9af4693006305b7500
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-03-19 23:18:29 +0100

    Add working reverse polish notation calculator

--- src/laboratory1/reversePolishNotationCalculator.erl
+++ src/laboratory1/reversePolishNotationCalculator.erl
@@ -1,54 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% @author radoj
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. Mar 2019 13:00
+%%%-------------------------------------------------------------------
 -module(reversePolishNotationCalculator).
 -author("jakub").
+
 %% API
--export([]).
 -export([rpn/1]).
 -export([calculate/2]).
--export([filter_list/1]).
-
-%%% 1 + 2 * 3 - 4 / 5 + 6 in rpn: 1 2 3 * 4 5 / - 6 + +
-%%% 1 + 2 + 3 + 4 + 5 + 6 * 7 in rpn: 1 2 + 3 + 4 + 5 + 6 7 * +
-%%% ((4 + 7) / 3) * (2 - 19) in rpn: 4 7 + 3 / 2 19 - *
-%%% 17 * (31 + 4) / ( (26 - 15) * 2 - 22 ) - 1 in rpn: 17 31 4 + * 26 15 - 2 * 22 - / 1 - *
+-export([filter_expression/1]).
 
 rpn(Expression) ->
-  calculate(filter_list(string:tokens(Expression, " ")), []).
+  calculate(filter_expression(string:tokens(Expression, " ")), []).
 
+calculate([], List) when length(List) == 1 -> hd(List);
+calculate([], []) -> "Empty string given! Try again";
+calculate([_], []) -> "Some operation needs to be done!";
+calculate([], _) -> "Stack is not empty!";
 calculate(["+" | T], [A, B | Stack]) ->
   calculate(T, [B + A | Stack]);
-calculate(["-" | T], [A, B, Stack]) ->
+calculate(["-" | T], [A, B | Stack]) ->
   calculate(T, [B - A | Stack]);
-calculate(["*" | T], [A, B, Stack]) ->
+calculate(["*" | T], [A, B | Stack]) ->
   calculate(T, [B * A | Stack]);
-calculate(["/" | T], [A, B, Stack]) ->
-  calculate(T, [B - A | Stack]);
-calculate(["^" | T], [A, B, Stack]) ->
+calculate(["/" | T], [A, B | Stack]) ->
+  calculate(T, [B / A | Stack]);
+calculate(["^" | T], [A, B | Stack]) ->
   calculate(T, [math:pow(B, A) | Stack]);
-calculate([], []) -> "Empty string given! Try again";
-calculate([], _) -> "Stack is not empty!";
-calculate([_], []) -> "Some operations left!";
-calculate([], List) when length(List) == 1 -> hd(List).
+calculate(["sqrt" | T], [A | Stack]) ->
+  calculate(T, [math:sqrt(A) | Stack]);
+calculate(["sin" | T], [A | Stack]) ->
+  calculate(T, [math:sin(A) | Stack]);
+calculate(["cos" | T], [A | Stack]) ->
+  calculate(T, [math:cos(A) | Stack]);
+calculate(["tan" | T], [A | Stack]) ->
+  calculate(T, [math:tan(A) | Stack]);
+calculate(["ctan" | T], [A | Stack]) ->
+  calculate(T, [1 / math:tan(A) | Stack]);
+calculate([H | Stack], L) ->
+  calculate(Stack, [H | L]).
 
-filter_list([]) -> [];
-filter_list([H | T]) when is_number(H) ->
-  [H | filter_list(T)];
-filter_list(["+" | T]) ->
-  ["+", filter_list(T)];
-filter_list(["-" | T]) ->
-  ["-", filter_list(T)];
-filter_list(["*" | T]) ->
-  ["*", filter_list(T)];
-filter_list(["/" | T]) ->
-  ["/", filter_list(T)];
-filter_list(["^" | T]) ->
-  ["^", filter_list(T)];
-%%filter_list(["sqrt" | T]) ->
-%%["sqrt", filter_list(T)];
-filter_list(["sin" | T]) ->
-  ["sin", filter_list(T)];
-filter_list(["cos" | T]) ->
-  ["cos", filter_list(T)];
-filter_list(["tan" | T]) ->
-  ["tan", filter_list(T)];
-filter_list(["ctan" | T]) ->
-  ["ctan", filter_list(T)].
+filter_expression([]) -> [];
+filter_expression(["+" | T]) ->
+  ["+" | filter_expression(T)];
+filter_expression(["-" | T]) ->
+  ["-" | filter_expression(T)];
+filter_expression(["/" | T]) ->
+  ["/" | filter_expression(T)];
+filter_expression(["*" | T]) ->
+  ["*" | filter_expression(T)];
+filter_expression(["^" | T]) ->
+  ["^" | filter_expression(T)];
+filter_expression(["sin" | T]) ->
+  ["sin" | filter_expression(T)];
+filter_expression(["cos" | T]) ->
+  ["cos" | filter_expression(T)];
+filter_expression(["tan" | T]) ->
+  ["tan" | filter_expression(T)];
+filter_expression(["ctan" | T]) ->
+  ["ctan" | filter_expression(T)];
+filter_expression(["sqrt" | T]) ->
+  ["sqrt" | filter_expression(T)];
+filter_expression([H | T]) ->
+  [list_to_integer(H) | filter_expression(T)].
