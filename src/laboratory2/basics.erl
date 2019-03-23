1dcc3ac6fa8cda68591842dba79be43a90d3e73e
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-03-23 22:22:42 +0100

    Add some list comprehension examples

--- src/laboratory2/basics.erl
+++ src/laboratory2/basics.erl
@@ -7,3 +7,12 @@
 %% LIST COMPREHENSIONS - examples for myself
 %% L = lists:seq(1, 100).
 %% List = [X || X <- L, X rem 2 == 0].
+%% Hello = fun(X) -> io:format("Hello ~s~n", [X]) end.
+
+%% Add1 = fun(X) -> X + 1 end.
+%% lists:map(Add1, [1,2,3]).
+%% lists:map(fun(X) -> X+2 end, [1,2,3]).
+%% lists:map(fun math:sqrt/1, [1,2,3]).
+
+%% Multi2 = fun(X) -> 2 * X end.
+%% lists:map(Multi2, [1,2,3]).
