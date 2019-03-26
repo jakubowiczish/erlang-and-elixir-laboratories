1c0cd6638fcd165c76f84bf6f6fadfb583db6577
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-03-26 12:12:27 +0100

    Add printing time of sortings: qsort:qs and lists:sort

--- src/laboratory2/qsort.erl
+++ src/laboratory2/qsort.erl
@@ -1,4 +1,3 @@
-
 -module(qsort).
 -author("jakub").
 
@@ -6,6 +5,9 @@
 -export([]).
 -export([lessThan/2]).
 -export([grtEqThan/2]).
+-export([qs/1]).
+-export([randomElems/3]).
+-export([compareSpeeds/3]).
 
 lessThan([], _) ->
   [];
@@ -16,3 +18,16 @@ grtEqThan([], _) ->
   [];
 grtEqThan(List, Arg) ->
   [X || X <- List, X >= Arg].
+
+qs([]) ->
+  [];
+qs([Pivot | Tail]) ->
+  qs(lessThan(Tail, Pivot)) ++ [Pivot] ++ qs(grtEqThan(Tail, Pivot)).
+
+randomElems(N, Min, Max) ->
+  [Min + rand:uniform(Max - Min) || X <- lists:seq(1, N)].
+
+compareSpeeds(List, Fun1, Fun2) ->
+  {Time1, _} = timer:tc(qsort, Fun1, [List]),
+  {Time2, _} = timer:tc(lists, Fun2, [List]),
+  io:fwrite("Time of sorting using qsort:qs: ~f seconds,~nTime of sorting using lists:sort: ~f seconds ~n", [Time1 / 1000000, Time2 / 1000000]).
