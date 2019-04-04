dafa3405a987f07e418b2a34ef668183ebde53b4
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-04 13:33:08 +0200

    Add more tests

--- src/laboratory3/airpollution/pollutionTest.erl
+++ src/laboratory3/airpollution/pollutionTest.erl
@@ -10,12 +10,26 @@ test_pollution_module() ->
   Monitor = pollution:createMonitor(),
   io:format("Created monitor: ~p~n", [Monitor]),
 
-  io:format("Adding new station: Krasinskiego, {100, 200} to this empty monitor~n"),
-  Monitor2 = pollution:addStation("Krasinskiego", {100, 200}, Monitor),
-  io:format("Actual state of monitor: ~p~n", [Monitor2]),
+  io:format("Adding new station: Broadway, {100, 200} to this empty monitor~n"),
+  Monitor1 = pollution:addStation("Broadway", {100, 200}, Monitor),
+  io:format("Actual state of monitor: ~p~n~n", [Monitor1]),
 
-  Monitor3 = pollution:addValue("Krasinskiego", "4-03-2019", "PM2.5", 20, Monitor2),
-  io:format("Actural state of monitor: ~p~n", [Monitor3]).
+  io:format("Adding new station: Carpentersville, {50, 51} ~n"),
+  Monitor2 = pollution:addStation("Carpentersville", {50, 51}, Monitor1),
+  io:format("Actual state of monitor: ~p~n~n", [Monitor2]),
+
+  io:format("Adding new station: WallStreet, {100,121} ~n"),
+  Monitor3 = pollution:addStation("WallStreet", {100, 121}, Monitor2),
+  io:format("Actual state of monitor: ~p~n~n", [Monitor3]),
+
+
+  io:format("Adding new value for Broadway: 4-03-2019, PM2.5, 20, ~n"),
+  Monitor4 = pollution:addValue("Broadway", "4-03-2019", "PM2.5", 20, Monitor3),
+  io:format("Actual state of monitor: ~p~n~n", [Monitor4]),
+
+  io:format("Adding new value for Carpentersville: 5-03-2019, SO2, 40, ~n"),
+  Monitor5 = pollution:addValue("Carpentersville", "5-03-2019", "SO2", 40, Monitor4),
+  io:format("Actual state of monitor: ~p~n~n", [Monitor5]).
 
 
 test_errors_detecting() ->
