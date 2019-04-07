560e447f499de3d0908749dffdd9c36ad3d0fa7b
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-07 21:28:18 +0200

    Add more testing methods

--- src/laboratory3/airpollution/pollutionTest.erl
+++ src/laboratory3/airpollution/pollutionTest.erl
@@ -3,13 +3,14 @@
 
 %% API
 -export([]).
--export([testWholePollutionModule/0, testErrorDetecting/0, testCreateMonitorMethod/0, testAddStationMethod/0, testAddValueMethod/0]).
+-export([testWholePollutionModule/0, testErrorDetecting/0, testCreateMonitorMethod/0, testAddStationMethod/0, testAddValueMethod/0, testRemoveValueMethod/0, testGetOneValueMethod/0]).
 
 
 testCreateMonitorMethod() ->
   io:format("TESTING CREATE MONITOR METHOD:~n"),
   EmptyMonitor = pollution:createMonitor(),
   io:format("Created monitor: ~p~n", [EmptyMonitor]).
+
 
 testAddStationMethod() ->
   io:format("TESTING ADD STATION METHOD:~n"),
@@ -20,11 +21,13 @@ testAddStationMethod() ->
   BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
   io:format("Actual state of monitor: ~p~n~n", [BroadwayMonitor]),
 
+  %% ERROR REPORT EXPECTED
   io:format("Adding station WITH THE SAME NAME AS ONE OF EXISTING STATIONS:
   Broadway, {101, 201}~n"),
   BroadwayMonitor2 = pollution:addStation("Broadway", {101, 201}, BroadwayMonitor),
   io:format("Actual state of monitor: ~p~n~n", [BroadwayMonitor2]),
 
+  %% ERROR REPORT EXPECTED
   io:format("Adding station WITH THE SAME COORDINATES AS ONE OF EXISTING STATIONS:
   LosAngeles, {100, 200}~n"),
   LosAngelesMonitor = pollution:addStation("LosAngeles", {100, 200}, BroadwayMonitor2),
@@ -32,7 +35,7 @@ testAddStationMethod() ->
 
 
 testAddValueMethod() ->
-  io:format("TESTING ADD STATION METHOD:~n"),
+  io:format("TESTING ADD VALUE METHOD:~n"),
   EmptyMonitor = pollution:createMonitor(),
   io:format("Created monitor: ~p~n", [EmptyMonitor]),
 
@@ -46,7 +49,54 @@ testAddValueMethod() ->
 
   io:format("Adding new value for station: Broadway, {100, 200} ~n"),
   ValueMonitor2 = pollution:addValue({100, 200}, "7-04-2019", "PM10", 32, ValueMonitor1),
-  io:format("Actual state of monitor: ~p~n~n", [ValueMonitor2]).
+  io:format("Actual state of monitor: ~p~n~n", [ValueMonitor2]),
+
+  %% ERROR REPORT EXPECTED
+  io:format("Adding new value for station: Broadway, {100, 200} ~n"),
+  ValueMonitor3 = pollution:addValue({100, 200}, "7-04-2019", "PM10", 32, ValueMonitor2),
+  io:format("Actual state of monitor: ~p~n~n", [ValueMonitor3]).
+
+
+testRemoveValueMethod() ->
+  io:format("TESTING REMOVE VALUE METHOD:~n"),
+  EmptyMonitor = pollution:createMonitor(),
+  io:format("Created monitor: ~p~n", [EmptyMonitor]),
+
+  io:format("Adding new station: Broadway, {100, 200} to this empty monitor~n"),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+  io:format("Actual state of monitor: ~p~n~n", [BroadwayMonitor]),
+
+  io:format("Adding new value for station: Broadway, {100, 200} ~n"),
+  ValueMonitor1 = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
+  io:format("Actual state of monitor: ~p~n~n", [ValueMonitor1]),
+
+  io:format("Removing value from station: Broadway, {100, 200}, 7-04-2019, PM10 ~n"),
+  RemoveValueMonitor1 = pollution:removeValue("Broadway", "7-04-2019", "PM10", ValueMonitor1),
+  io:format("Actual state of monitor: ~p~n~n", [RemoveValueMonitor1]).
+
+
+testGetOneValueMethod() ->
+  io:format("TESTING GET ONE VALUE METHOD:~n"),
+  EmptyMonitor = pollution:createMonitor(),
+  io:format("Created monitor: ~p~n", [EmptyMonitor]),
+
+  io:format("Adding new station: Broadway, {100, 200} to this empty monitor~n"),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+  io:format("Actual state of monitor: ~p~n~n", [BroadwayMonitor]),
+
+  io:format("Adding new value for station: Broadway, {100, 200} ~n"),
+  ValueMonitor1 = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
+  io:format("Actual state of monitor: ~p~n~n", [ValueMonitor1]),
+
+  io:format("Getting one value from station: Broadway, {100, 200} ~n"),
+  ValueFromMonitor1 = pollution:getOneValue("Broadway", "7-04-2019", "PM10", ValueMonitor1),
+  io:format("Value from monitor: ~p~n~n", [ValueFromMonitor1]),
+
+  %% ERROR REPORT EXPECTED - WRONG DATE
+  io:format("Getting one value from station: Broadway, {100, 200} ~n"),
+  ValueFromMonitor2 = pollution:getOneValue("Broadway", "20-04-2019", "PM10", ValueMonitor1),
+  io:format("Value from monitor: ~p~n~n", [ValueFromMonitor2]).
+
 
 
 
