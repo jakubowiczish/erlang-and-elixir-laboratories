7244d8abe91dcf46088cdf0d904e081b91fad653
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-29 00:40:35 +0200

    Fix pingpong module

--- src/laboratory3/pingpong/pingpong.erl
+++ src/laboratory3/pingpong/pingpong.erl
@@ -1,13 +1,18 @@
 -module(pingpong).
 -author("jakub").
 
+
 %% API
--export([start/0, initialize/0]).
+-export([start/0, stop/0, play/1]).
+
+%% pingpong:start().
+%% pingpong:play(10).
+%% pingpong:stop().
 
 
 start() ->
   case (lists:member(ping, registered())) or (lists:member(pong, registered())) of
-    true -> error_logger:error_msg("The process is ALREADY RUNNING");
+    true -> error_logger:error_msg("The process is ALREADY RUNNING ~n");
     false ->
       PingPid = spawn(fun() -> initialize() end),
       PongPid = spawn(fun() -> initialize() end),
@@ -18,29 +23,42 @@ start() ->
   end.
 
 
-initialize() ->
+stop() ->
+  case (lists:member(ping, registered())) or (lists:member(pong, registered())) of
+    false -> error_logger:error_msg("Ping pong is not running - NOTHING TO TERMINATE ~n");
+    true ->
+      ping ! {self(), terminate},
+      pong ! {self(), terminate}
+  end.
+
+
+play(N) ->
+  pong ! {self(), play, N}.
+
+
+loop(Pid) ->
   receive
-    To -> loop(To)
+    {From, play, N} when N > 0 ->
+      Who = case Pid of
+              ping -> "Ping";
+              pong -> "Pong"
+            end,
+      io:format("~s is now equal to: ~B~n from: ~p ~n", [Who, N, From]),
+      Pid ! {self(), play, N - 1},
+      loop(Pid);
+    {From, terminate} ->
+      io:format("Received TERMINATION signal from: ~p~n", [From]),
+      ok
   after 20000 ->
     io:format("TIMEOUT ~n"),
     timeout
   end.
 
 
-loop(Pid) ->
+initialize() ->
   receive
-    {From, N} when N > 0 ->
-      Who = case Pid of
-              ping -> "Ping";
-              pong -> "Pong"
-            end,
-      io:format("~s is now equal to: ~B~n from: ~p ~n", [Who, N, From]),
-      Pid ! {self(), N - 1},
-      loop(Pid);
-    {From, terminate} ->
-      io:format("Received TERMINATION signal from: ~p~n", [From]),
-      ok
-  after 10000 ->
+    To -> loop(To)
+  after 20000 ->
     io:format("TIMEOUT ~n"),
     timeout
   end.
