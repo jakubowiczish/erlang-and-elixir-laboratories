7af34a1babe9b901fcba97bfffd62eb12b0bb2d1
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-05-01 21:58:57 +0200

    Add more EUnit tests for pollution module

--- src/laboratory2/pollution/pollution_EUnit_tests.erl
+++ src/laboratory2/pollution/pollution_EUnit_tests.erl
@@ -5,16 +5,29 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([
+  runAllTests/0,
   testCreateMonitorMethod/0,
   testAddStationMethod/0,
-  runAllTests/0
-  , testAddValueMethod/0]).
+  testAddValueMethod/0,
+  testRemoveValueMethod/0,
+  testGetOneValueMethod/0,
+  testGetStationMeanMethod/0,
+  testGetDailyMeanMethod/0]).
 
 
 runAllTests() ->
   testCreateMonitorMethod(),
   testAddStationMethod(),
-  testAddValueMethod().
+  testAddValueMethod(),
+  testRemoveValueMethod(),
+  testGetOneValueMethod(),
+  testGetStationMeanMethod(),
+  testGetDailyMeanMethod().
+
+
+
+
+
 
 testCreateMonitorMethod() ->
   EmptyMonitor = pollution:createMonitor(),
@@ -27,30 +40,81 @@ testCreateMonitorMethod() ->
 
 testAddStationMethod() ->
   EmptyMonitor = pollution:createMonitor(),
-  ActualStation = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
 
-  ExpectedStation = {monitor, #{{100, 200} =>{station, "Broadway", {100, 200}},
+  ActualMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+  ExpectedMonitor = {monitor, #{{100, 200} =>{station, "Broadway", {100, 200}},
     "Broadway" =>{station, "Broadway", {100, 200}}}, #{}},
 
-  ?assertEqual(
-    ExpectedStation,
-    ActualStation
-  ).
+  ?assertEqual(ExpectedMonitor, ActualMonitor).
 
 
 
 testAddValueMethod() ->
   EmptyMonitor = pollution:createMonitor(),
   BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
-  ValueMonitor = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
 
-  ?assertEqual(
-    {monitor,
-    #{{100, 200} => {station, "Broadway", {100, 200}},
-      "Broadway" => {station, "Broadway", {100, 200}}},
-      #{{station, "Broadway", {100, 200}} =>[{measurement, "7-04-2019", "PM10", 16}]}},
+  ActualMonitor = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
+  ExpectedMonitor = {monitor, #{{100, 200} => {station, "Broadway", {100, 200}},
+    "Broadway" => {station, "Broadway", {100, 200}}},
+    #{{station, "Broadway", {100, 200}} =>[{measurement, "7-04-2019", "PM10", 16}]}},
 
-    ValueMonitor
-  ).
+  ?assertEqual(ExpectedMonitor, ActualMonitor).
+
+
+testRemoveValueMethod() ->
+  EmptyMonitor = pollution:createMonitor(),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+
+  MonitorWithValue = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
+
+  ActualMonitor = pollution:removeValue("Broadway", "7-04-2019", "PM10", MonitorWithValue),
+  ExpectedMonitor = {monitor, #{{100, 200} => {station, "Broadway", {100, 200}},
+    "Broadway" =>{station, "Broadway", {100, 200}}},
+    #{{station, "Broadway", {100, 200}} => []}},
+
+  ?assertEqual(ExpectedMonitor, ActualMonitor).
+
+
+
+testGetOneValueMethod() ->
+  EmptyMonitor = pollution:createMonitor(),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+
+  MonitorWithValue = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, BroadwayMonitor),
+  ActualValue = pollution:getOneValue("Broadway", "7-04-2019", "PM10", MonitorWithValue),
+
+  ?assertEqual(16, ActualValue).
+
+
+testGetStationMeanMethod() ->
+  EmptyMonitor = pollution:createMonitor(),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+  ChicagoMonitor = pollution:addStation("Chicago", {101, 201}, BroadwayMonitor),
+
+  MonitorWithValue = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, ChicagoMonitor),
+  MonitorWithValue2 = pollution:addValue("Broadway", "8-04-2019", "PM10", 32, MonitorWithValue),
+  MonitorWithValue3 = pollution:addValue("Chicago", "8-04-2019", "PM10", 96, MonitorWithValue2),
+
+  ActualValue = pollution:getStationMean("Broadway", "PM10", MonitorWithValue3),
+
+  ?assertEqual(24.0, ActualValue).
+
+
+testGetDailyMeanMethod() ->
+  EmptyMonitor = pollution:createMonitor(),
+  BroadwayMonitor = pollution:addStation("Broadway", {100, 200}, EmptyMonitor),
+  ChicagoMonitor = pollution:addStation("Chicago", {101, 201}, BroadwayMonitor),
+
+  MonitorWithValue = pollution:addValue("Broadway", "7-04-2019", "PM10", 16, ChicagoMonitor),
+  MonitorWithValue2 = pollution:addValue("Broadway", "8-04-2019", "PM10", 32, MonitorWithValue),
+  MonitorWithValue3 = pollution:addValue("Chicago", "8-04-2019", "PM10", 96, MonitorWithValue2),
+
+  ActualValue = pollution:getDailyMean("8-04-2019", "PM10", MonitorWithValue3),
+
+  ?assertEqual(64.0, ActualValue).
+
+
+
+
 
 
