26119e4783bbf9ee9e0e2df193b7be6967409ea6
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-05-19 20:12:14 +0200

    Add remaining test methods for gen server module

--- src/laboratory4/pollution_gen_server/pollution_gen_server_EUnit_tests.erl
+++ src/laboratory4/pollution_gen_server/pollution_gen_server_EUnit_tests.erl
@@ -6,18 +6,11 @@
 
 
 -export([
-  testAllGenServerMethods/0,
   testAddStationGenServerMethod/0,
   testAddValueGenServerMethod/0,
-  testRemoveValueGenServerMethod/0
-]).
+  testRemoveValueGenServerMethod/0,
+  testGetOneValueGenServerMethod/0, testGetStationMeanGenServerMethod/0, testGetDailyMeanGenServerMethod/0, testImportFromCsvGenServerMethod/0]).
 
-
-
-testAllGenServerMethods() ->
-  testAddStationGenServerMethod(),
-  testAddValueGenServerMethod(),
-  testRemoveValueGenServerMethod().
 
 
 
@@ -62,17 +55,14 @@ testRemoveValueGenServerMethod() ->
 
   ExpectedResult = {monitor, #{{100, 200} => {station, "Broadway", {100, 200}},
     "Broadway" => {station, "Broadway", {100, 200}}},
-    #{{station, "Broadway", {100, 200}} =>
-    [{measurement, "7-04-2019", "PM10", 16}]}},
+    #{{station, "Broadway", {100, 200}} =>[{measurement, "7-04-2019", "PM10", 16}]}},
 
   ?assertEqual(ExpectedResult, ActualResult),
 
   ActualResult2 = pollution_gen_server:removeValue("Broadway", "7-04-2019", "PM10"),
 
-  ExpectedResult2 = {monitor, #{{100, 200} =>
-  {station, "Broadway", {100, 200}},
-    "Broadway" =>
-    {station, "Broadway", {100, 200}}},
+  ExpectedResult2 = {monitor, #{{100, 200} =>{station, "Broadway", {100, 200}},
+    "Broadway" =>{station, "Broadway", {100, 200}}},
     #{{station, "Broadway", {100, 200}} => []}},
 
   ?assertEqual(ExpectedResult2, ActualResult2),
@@ -80,3 +70,66 @@ testRemoveValueGenServerMethod() ->
   pollution_gen_server:stop().
 
 
+
+testGetOneValueGenServerMethod() ->
+  pollution_gen_server:start(pollution:createMonitor()),
+
+  pollution_gen_server:addStation("Broadway", {100, 200}),
+
+  pollution_gen_server:addValue("Broadway", "7-04-2019", "PM10", 16),
+
+  ActualResult = pollution_gen_server:getOneValue("Broadway", "7-04-2019", "PM10"),
+
+  ?assertEqual(16, ActualResult),
+
+  pollution_gen_server:stop().
+
+
+
+testGetStationMeanGenServerMethod() ->
+  pollution_gen_server:start(pollution:createMonitor()),
+
+  pollution_gen_server:addStation("Broadway", {100, 200}),
+
+  pollution_gen_server:addValue("Broadway", "7-04-2019", "PM10", 16),
+
+  pollution_gen_server:addValue("Broadway", "7-04-2019", "PM10", 48),
+
+  ActualResult = pollution_gen_server:getStationMean("Broadway", "PM10"),
+
+  ?assertEqual(32.0, ActualResult),
+
+  pollution_gen_server:stop().
+
+
+testGetDailyMeanGenServerMethod() ->
+  pollution_gen_server:start(pollution:createMonitor()),
+
+  pollution_gen_server:addStation("Broadway", {100, 200}),
+
+  pollution_gen_server:addValue("Broadway", "8-04-2019", "PM10", 16),
+
+  pollution_gen_server:addValue("Broadway", "8-04-2019", "PM10", 48),
+
+  ActualResult = pollution_gen_server:getDailyMean("8-04-2019", "PM10"),
+
+  ?assertEqual(32.0, ActualResult),
+
+  pollution_gen_server:stop().
+
+
+
+testImportFromCsvGenServerMethod() ->
+  pollution_gen_server:start(pollution:createMonitor()),
+
+  ActualResult = pollution_gen_server:importFromCsv("/home/jakub/IdeaProjects/ErlangLaboratories/src/laboratory4/pollution_gen_server/data.csv"),
+
+  ExpectedResult = {monitor,
+    #{{365, 366} => {station, "NewYork", {365, 366}},
+      "NewYork" => {station, "NewYork", {365, 366}}},
+    #{{station, "NewYork", {365, 366}} =>
+    [{measurement, "6-03-2019", "PM10", 20}]}},
+
+  ?assertEqual(ExpectedResult, ActualResult),
+
+  pollution_gen_server:stop().
